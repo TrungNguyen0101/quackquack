01908d25e93eb117400061a4d093d6f1412784c0
TrungNguyen0101 <tronzinmj213@gmail.com>
2024-06-21 22:39:05 +0700

    123123

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ACCESS_TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE1MTE5OSwidGltZXN0YW1wIjoxNzE4OTY4ODIwOTkxLCJ0eXBlIjoxLCJpYXQiOjE3MTg5Njg4MjAsImV4cCI6MTcxOTU3MzYyMH0.U4VtIuA0piy7uFIFa5FkhJbFToqUQ1P5IXQszEfDmQA";
 const collectGoldenDuck = require("./scripts/collectGoldenDuck");
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json("Welcome, your app is working well");
   collectGoldenDuck(ACCESS_TOKEN);
 });
