eb78a75b6c1f3ae46c067dc097bfa68d15bb0f3d
TrungNguyen0101 <tronzinmj213@gmail.com>
2024-06-21 23:00:09 +0700

    123123

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app = express();
 const loadToken = require("./modules/loadToken");
 const collectGoldenDuck = require("./scripts/collectGoldenDuck");
+var cron = require("node-cron");
 
 app.get("/", (req, res) => {
   res.status(200).json("Welcome, your app is working well");
 });
-collectGoldenDuck(
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE1MTE5OSwidGltZXN0YW1wIjoxNzE4OTY4ODIwOTkxLCJ0eXBlIjoxLCJpYXQiOjE3MTg5Njg4MjAsImV4cCI6MTcxOTU3MzYyMH0.U4VtIuA0piy7uFIFa5FkhJbFToqUQ1P5IXQszEfDmQA"
-);
-
+cron.schedule("* * * * *", () => {
+  collectGoldenDuck(
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE1MTE5OSwidGltZXN0YW1wIjoxNzE4OTY4ODIwOTkxLCJ0eXBlIjoxLCJpYXQiOjE3MTg5Njg4MjAsImV4cCI6MTcxOTU3MzYyMH0.U4VtIuA0piy7uFIFa5FkhJbFToqUQ1P5IXQszEfDmQA"
+  );
+});
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
