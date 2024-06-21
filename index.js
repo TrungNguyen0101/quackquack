5ea2ccff52484864f397554f94b49a1fd66e62eb
TrungNguyen0101 <tronzinmj213@gmail.com>
2024-06-22 00:18:29 +0700

    123123

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
-const app = express();
-const loadToken = require("./modules/loadToken");
+const cron = require("node-cron");
 const collectGoldenDuck = require("./scripts/collectGoldenDuck");
-var cron = require("node-cron");
+const app = express();
+const ACCESS_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE1MTE5OSwidGltZXN0YW1wIjoxNzE4OTY4ODIwOTkxLCJ0eXBlIjoxLCJpYXQiOjE3MTg5Njg4MjAsImV4cCI6MTcxOTU3MzYyMH0.U4VtIuA0piy7uFIFa5FkhJbFToqUQ1P5IXQszEfDmQA";
 
 app.get("/", (req, res) => {
   res.status(200).json("Welcome, your app is working well");
-  cron.schedule("*/10 * * * * *", () => {});
 });
-collectGoldenDuck(
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE1MTE5OSwidGltZXN0YW1wIjoxNzE4OTY4ODIwOTkxLCJ0eXBlIjoxLCJpYXQiOjE3MTg5Njg4MjAsImV4cCI6MTcxOTU3MzYyMH0.U4VtIuA0piy7uFIFa5FkhJbFToqUQ1P5IXQszEfDmQA"
-);
-app.listen(3000, () => console.log("Server ready on port 3000."));
+
+collectGoldenDuck(ACCESS_TOKEN);
+app.listen(3000, () => {
+  console.log("Server ready on port 3000.");
+});
 
 module.exports = app;
