c0fe088b9382ae4aadb8aeb0666de4ed3a4f8418
TrungNguyen0101 <tronzinmj213@gmail.com>
2024-06-21 22:41:37 +0700

    1231231

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const collectGoldenDuck = require("./scripts/collectGoldenDuck");
 
 app.get("/", (req, res) => {
   res.status(200).json("Welcome, your app is working well");
-  collectGoldenDuck(ACCESS_TOKEN);
+  collectGoldenDuck(
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE1MTE5OSwidGltZXN0YW1wIjoxNzE4OTY4ODIwOTkxLCJ0eXBlIjoxLCJpYXQiOjE3MTg5Njg4MjAsImV4cCI6MTcxOTU3MzYyMH0.U4VtIuA0piy7uFIFa5FkhJbFToqUQ1P5IXQszEfDmQA"
+  );
 });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
